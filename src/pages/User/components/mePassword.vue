4174b74431ccd4cee322366b3b98d6a77f9803f6
YOU <youwg@suninfo.com>
2018-03-06 14:00:59 +0800

    个人中心

--- src/pages/User/components/mePassword.vue
+++ src/pages/User/components/mePassword.vue
@@ -2,11 +2,39 @@
 create by YOU
 */
 <template>
-  <div>密码修改</div>
+  <div>
+    <div style="max-width: 500px; margin: 50px auto 0">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="旧密码">
+          <el-input v-model="form.oldPassword" type="password"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码">
+          <el-input v-model="form.newPassword" type="password"></el-input>
+        </el-form-item>
+        <el-form-item label="重复密码">
+          <el-input v-model="form.repeatPassword" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">保存修改</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script type="text/babel">
-  export default {};
+  export default {
+    data() {
+      return {
+        form: {
+          oldPassword: '',
+          newPassword: '',
+          repeatPassword: ''
+        }
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
